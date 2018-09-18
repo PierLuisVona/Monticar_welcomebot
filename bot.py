be450095b6120d8e5e507f039703e7f4e7ff1cf0
pier <pg.vona@gmail.com>
2018-09-18 23:55:06 +0200

    prova heroku

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
         for upd in updates:
             if ('new_chat_member' in upd["message"].keys()):
-                send_message(upd['message']['chat']['id'],"benvenutissimo!!")
+                send_message(upd['message']['chat']['id'],"buongiornissimo!!")
 
 
         print(updates)
