5558265238aed2098b90c4372bcffe4c999bca5b
pier <pg.vona@gmail.com>
2018-09-18 23:47:50 +0200

    nuova repo

---
+++ bot.py
@@ -0,0 +1,35 @@
+import requests
+import datetime
+import os
+
+token = os.environ.get('TOKEN',None)
+api_url = "https://api.telegram.org/bot{}/".format(token)
+
+
+def get_updates(offset=None, timeout=30):
+    resp = requests.get(api_url + 'getUpdates', {'timeout': timeout, 'offset': offset})
+    result_json = resp.json()['result']
+    return result_json
+
+def send_message(chat_id, text):
+    params = {'chat_id': chat_id, 'text': text}
+    method = 'sendMessage'
+    resp = requests.post(api_url + method, params)
+    return resp
+
+
+if __name__ == '__main__':
+    new_offset = None
+
+    while True:
+        updates = get_updates(new_offset)
+
+        for upd in updates:
+            if ('new_chat_member' in upd["message"].keys()):
+                send_message(upd['message']['chat']['id'],"benvenutissimo!!")
+
+
+        print(updates)
+
+        if (len(updates)>0):
+            new_offset = updates[-1]['update_id'] + 1
