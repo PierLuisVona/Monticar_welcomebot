40f3fb02ac7250ae949b40b22f52c15c7982e692
pier <pg.vona@gmail.com>
2018-09-24 12:17:47 +0200

    update messagge

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
         for upd in updates:
             if ('new_chat_member' in upd["message"].keys()):
-                send_message(upd['message']['chat']['id'],"benvenuto "+upd['message']['new_chat_member']['first_name']+"!")
+                send_message(upd['message']['chat']['id'],"Ciao "+upd['message']['new_chat_member']['first_name']+"! In questa chat potrei offrire e chiedere un passaggio diretto a scuola, mi raccomando PROPOSITIVITÁ ed EDUCAZIONE! buon viaggio :)")
 
 
         print(updates)
